44359cf96d1a4ed2f14ce57928266a2621a9749c
Ian Rodriguez <ian.rgz@gmail.com>
2015-05-01 16:26:40 -0700

    Initial commit

---
+++ chat-test.js
@@ -0,0 +1,46 @@
+Users = new Mongo.Collection('users');
+Messages = new Mongo.Collection('messages');
+
+Router.route('/', function () {
+  var loginError = '';
+
+  this.render('home');
+
+  Template.home.events({
+    'submit .login-form': function (event) {
+      var username = $(event.target).find("#username")[0].value;
+      var password = $(event.target).find("#password")[0].value;
+
+      var user = Users.findOne({
+        username: username
+      });
+
+      if (user.password === password) {
+        Router.go('messages');
+      } else {
+        error = 'wrong password';
+      }
+
+      console.log(user);
+      return false;
+    }
+  })
+});
+
+Router.route('/messages', function () {
+  this.render('messages', {
+    data: function () {
+      return Messages.find();
+    }
+  });
+})
+
+if (Meteor.isClient) {
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
